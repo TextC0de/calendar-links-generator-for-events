5e40cbe3193881fbf3b7996e7c4e48e8923cbf1e
Ignacio Guzman <ignacio658mg@gmail.com>
2020-09-11 22:41:37 -0300

    Express updated for production

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import path from 'path';
 import icsRoutes from './routes';
 
 const PORT = process.env.PORT || 5000;
@@ -10,7 +11,15 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use('/ics', icsRoutes);
-app.use('/ics', express.static('public'));
+app.use('/ics', express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+if (process.env.NODE_ENV === 'production') {
+    app.get('/*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    });
+}
 
 app.listen(PORT, (err) => {
     if (err) throw err;
